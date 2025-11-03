61b52a3faa331d3e6b36de79bfd3dd936b070571
JJ-Dynamite <joelmulammoottil994@gmail.com>
2025-11-03 18:52:34 +0530

    [fix]: Fix Coolify deployment config

--- src/components/admin/charts/index.js
+++ src/components/admin/charts/index.js
@@ -1,5 +1,5 @@
-export { default as ActivityLineChart } from './ActivityLineChart';
-export { default as UserActivityBreakdown } from './UserActivityBreakdown';
-export { default as AnalyticsChart } from './AnalyticsChart';
-export { default as SystemHealthIndicator } from './SystemHealthIndicator';
-export { default as PerformanceMetrics } from './PerformanceMetrics'; 
+export { default as ActivityLineChart } from './ActivityLineChart.jsx';
+export { default as UserActivityBreakdown } from './UserActivityBreakdown.jsx';
+export { default as AnalyticsChart } from './AnalyticsChart.jsx';
+export { default as SystemHealthIndicator } from './SystemHealthIndicator.jsx';
+export { default as PerformanceMetrics } from './PerformanceMetrics.jsx'; 
